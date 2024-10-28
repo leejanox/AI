042fa7d474fa526df5f2e5f1e1fab0e1397b27b9
leejanox <162652877+leejanox@users.noreply.github.com>
2024-10-28 17:22:22 +0900

    second commit

--- croll2.py
+++ croll2.py
@@ -5,8 +5,9 @@ from selenium.webdriver.common.by import By
 import time
 import pandas as pd
 
+
 #엑셀파일 읽어서 상품번호를 가져오기
-df = pd.read_excel("economy.xlsx")
+df = pd.read_excel("종합베스트.xlsx")
 
 pd_ids= df["상품번호"].tolist()
 
@@ -14,30 +15,10 @@ pd_ids= df["상품번호"].tolist()
 driver = webdriver.Chrome()
     
 # CSV 파일 생성
-with open('Info.csv', mode='w', newline='', encoding='utf-8') as file:
+with open('InfoAll.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     # 컬럼명 작성
-    writer.writerow(["Name", "Author", "Publisher", "Rating", "Review Count", "Sale Rank", "Original Price", "Sale Price"])
-
-             
-    #요소 찾을 문장 만들기
-    def make(col,classname):
-        [datas]=[]
-        for i in range(len(classnames)):
-            datas[i]=f"{col[i]} = driver.find_element(By.CLASS_NAME, {classname[i]}).text"
-
-    for pd_id in pd_ids:
-        #웹사이트 접속 url 바꾸기
-        url= f"https://www.yes24.com/Product/Goods/{pd_id}"
-        # 웹사이트 접속
-        driver.get(url)
-        time.sleep(5)  # 페이지 로딩 대기
-    
-# CSV 파일 생성
-with open('Info.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    # 컬럼명 작성
-    writer.writerow(["Name", "Author", "Publisher", "Rating", "Review Count", "Sale Rank", "Original Price", "Sale Price"])
+    writer.writerow(["Name", "Author", "Publisher", "Rating", "Review Count", "Sale Rank", "Original Price", "Sale Price","category"])
 
     for pd_id in pd_ids:
         #웹사이트 접속 url 바꾸기
@@ -56,16 +37,26 @@ with open('Info.csv', mode='w', newline='', encoding='utf-8') as file:
             #찾을 데이터 classname
             classnames = ["gd_name",'gd_auth','gd_pub','yes_b','txC_blue',"gd_sellNum","yes_m","nor_price"]
 
-            for classname in classnames:
+            for i in range(len(classnames)):
                 try:
-                    data = driver.find_element(By.CLASSNAME,classname)
+                    data = driver.find_element(By.CLASS_NAME,classnames[i]).text
                     #만약 데이터가 찾은 데이터가 있으면 data_found = True
-                    if data and data != "N/A":
-                        datas.append(data)
+                    datas.append(data)
+                    data_found = True
+                    print(f'{i} : {data}')
                 except:
                     datas.append("N/A")
 
-            if data_found:
+            #classname으로 안긁힘
+            try:
+                dataXpath = driver.find_element(By.XPATH,"/html/body/div/div[3]/div/div[3]/a").text
+                datas.append(dataXpath)
+                data_found=True
+                print(dataXpath)
+            except:
+                datas.append("N/A")
+
+            if data_found == True:
                 #데이터가 정상적으로 있으면 CSV 파일에 기록
                 writer.writerow(datas)
                 print("저장완료")
